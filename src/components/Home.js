b4434e56325e88aaf1ac0eb704262703dd016433
CPenalba <carolinapenalbacorpa@gmail.com>
2024-10-21 13:23:30 +0200

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home Component</h1>
+        <img src="https://casasinhaus.com/wp-content/uploads/2020/03/casa-modular-hormigon-premium-mallorca-en-mallorca-1150x646.jpg"
+        style={{width: "150px", height:"150px"}}></img>
+      </div>
+    )
+  }
+}
